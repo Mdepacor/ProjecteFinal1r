02d3b9645a9a8a4c4c6955f2dbd34c53f08845b5
ismaDLR <ismadelosreyes@gmail.com>
2024-05-14 16:39:20 +0200

    Vida

--- Assets/Scripts/Enemigos.cs
+++ Assets/Scripts/Enemigos.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemigos : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    int vida;
+    //GameObject barra;
     void Start()
     {
-        
+        vida = 1;
+        //barra = transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (vida < 1) 
+        { 
+            Destroy(gameObject);
         
+        
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,6 +31,9 @@ public class Enemigos : MonoBehaviour
         if (collision.gameObject.tag == "Finish")
         {
             Destroy(gameObject);
+         
+            
+
         }
     }
 }
