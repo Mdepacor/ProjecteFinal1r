07e01f1110b8c4f77b7984103c5ba8ea2b4384c1
Mdepacor <165807392+Mdepacor@users.noreply.github.com>
2024-05-28 18:11:45 +0200

    Creación escena MenuInicio

---
+++ Assets/Scripts/MenuInicio/MenuPresenter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPresented : MonoBehaviour
+{
+    [SerializeField] MenuModel menu;
+
+    [SerializeField] Button jugar;
+    [SerializeField] Button opciones;
+    [SerializeField] Button salir;
+}
