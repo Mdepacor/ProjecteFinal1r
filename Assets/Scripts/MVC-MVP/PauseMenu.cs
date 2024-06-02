775da2b2586c6c4dd10dddb0d9239e04a7823e9a
Mdepacor <165807392+Mdepacor@users.noreply.github.com>
2024-06-02 15:42:11 +0200

    Arreglo Bug vida
    Arreglo del bug donde se veia lavida por encima del menu de pause

--- Assets/Scripts/MVC-MVP/PauseMenu.cs
+++ Assets/Scripts/MVC-MVP/PauseMenu.cs
@@ -51,6 +51,7 @@ public class Pausemenu : MonoBehaviour
     public void mostrarPausePanel()
     {
         pausePanel.SetActive(true);
+        pausePanel.transform.SetAsLastSibling();
         Time.timeScale = 0;
     }
 
